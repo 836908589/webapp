6bbb112cd0624f754d0e567ee744019a4e91623f
836908589 <836908589@qq.com>
2016-10-31 18:06:59 +0800

    20161031-18:06

--- js/kursenko/home.js
+++ js/kursenko/home.js
@@ -12,5 +12,12 @@ function reH() {
     $("header").css("height",winH);
     $(".banner_mp4").css({"height":winH,"width":winW});
     console.log(winW);
+    if(winW<=768){
+        $(".se_p").html("Alex is an awesome designer with a fine sense of style." +
+            " Over the 8 years of working with our company, he has completed"+
+        "a vast number of projects for us, and each of them exceeded my expectations."+
+        "I recommend Alex as a highly perceptive designer who always knows"+
+        "precisely what the customer wants.")
+    }
 }
 
