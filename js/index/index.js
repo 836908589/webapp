00fb27de022380f0c773cb6aac6df3b4a9674c9c
836908589 <836908589@qq.com>
2016-11-01 15:33:16 +0800

    20161101-15:33

--- js/index/index.js
+++ js/index/index.js
@@ -1,32 +1,34 @@
 /**
  * Created by ThinkPad User on 2016/10/27.
  */
-var url;
-function resizeH(){
-   var winH=$(window).height();
-   $(".bg").css({"height":winH,"min-height":"740px"});
-}
-resizeH();
+reH();
 $(window).resize(function () {
-   resizeH();
+    reH();
 })
+function reH() {
+    var winW = $(window).width();
+    console.log(winW);
+
+}
+
+
 /*检测IE6,7,8*/
 $(function () {
-   var browser=navigator.appName
-   var b_version=navigator.appVersion
-   var version=b_version.split(";");
-   var trim_Version=version[1].replace(/[ ]/g,"");
-   if(browser=="Microsoft Internet Explorer" && trim_Version=="MSIE6.0")
-   {
-      document.write("你的IE浏览器版本过低，为了您更好的体验请现代浏览器（谷歌浏览器 (Chrome)，火狐 (Firefox)4.0+，Internet Explorer 9.0+)打开。");
-   }
-   else if(browser=="Microsoft Internet Explorer" && trim_Version=="MSIE7.0")
-   {
-      document.write("你的IE浏览器版本过低，为了您更好的体验请现代浏览器（谷歌浏览器 (Chrome)，火狐 (Firefox)4.0+，Internet Explorer 9.0+)打开。");
-   }
-   else if(browser=="Microsoft Internet Explorer" && trim_Version=="MSIE8.0")
-   {
-      document.write("你的IE浏览器版本过低，为了您更好的体验请现代浏览器（谷歌浏览器 (Chrome)，火狐 (Firefox)4.0+，Internet Explorer 9.0+)打开。");
-   }
+    var browser = navigator.appName
+    var b_version = navigator.appVersion
+    var version = b_version.split(";");
+    var trim_Version = version[1].replace(/[ ]/g, "");
+    var notice="<p style='font-size: 26px'>你的IE浏览器版本过低，为了您更好的体验请现代浏览器（谷歌浏览器 (Chrome)，火狐 (Firefox)4.0+，Internet Explorer 9.0+)打开。</p>";
+    if (browser == "Microsoft Internet Explorer" && trim_Version == "MSIE6.0") {
+        document.write(notice);
+    }
+    else if (browser == "Microsoft Internet Explorer" && trim_Version == "MSIE7.0") {
+        document.write(notice);
+    }
+    else if (browser == "Microsoft Internet Explorer" && trim_Version == "MSIE8.0") {
+        document.write(notice);
+    }else if(browser == "Microsoft Internet Explorer" && trim_Version == "MSIE9.0") {
+       /* alert("9")*/
+    }
+});
 
-})
--- js/load/load.js
+++ js/load/load.js
@@ -3,12 +3,13 @@
  */
 var myurl=["../page/kursenko/home.html","../page/wish/wish.html"];
 function myDelay(url) {
+    /*动画执行3s后消失*/
     setTimeout(function () {
         $(".load_animate").css({"opacity": "0", "transition": "all 1s linear"})
     }, 3000);
+    /*延迟4s自动跳转*/
     setTimeout(function () {
         window.location = url;
-        /*../page/kursenko/home.html*/
     }, 4000);
     return;
 }
