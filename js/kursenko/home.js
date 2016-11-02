a9eda9b11fe10c561cc1bfa123e2179069c60e56
836908589 <836908589@qq.com>
2016-11-02 17:00:05 +0800

    20161102-17:00

--- js/kursenko/home.js
+++ js/kursenko/home.js
@@ -24,29 +24,50 @@ function reH() {
 $(document).ready(function () {
     $(window).scroll(function () {
         var top = $(window).scrollTop();//获取当前位置离页面顶部的距离
-        var sec_Top = $(".second_scroll").offset().top;//获取元素离页面顶部的距离;
-        var for_Top = $(".fourth_scroll").offset().top-$(".fourth_scroll").height()/2;
+        var sec_Top = $(".second_scroll").offset().top- $(".third_scroll").height() / 2//获取元素离页面顶部的距离;
+        var thi_Top = $(".third_scroll").offset().top - $(".third_scroll").height() / 2;
+        var for_Top = $(".fourth_scroll").offset().top - $(".fourth_scroll").height() / 2;
+        var fif_Top = $(".fifth_scroll").offset().top - $(".fifth_scroll").height() / 2;
+        var six_Top = $(".sixth_scroll").offset().top - $(".sixth_scroll").height() / 2;
+        var seventh_part = $(".seventh_part").offset().top - $(".seventh_part").height() / 2;
+
+
+        function myAni(partTop,tit,discr,skip,img) {
+            var classTit = tit;
+            classTit = classTit.substr(1);
+
+            var classDiscr = discr;
+            classDiscr = classDiscr.substr(1);
+
+            var classSkip = skip;
+            classSkip = classSkip.substr(1);
+
+            if (top >= partTop) {
+                $(tit).removeClass(classTit).css("transition", "all 0.6s linear");
+             setInterval(function () {
+                    $(discr).removeClass(classDiscr).css("transition", "all 0.3s linear");
+                }, 1000);
+              setInterval(function () {
+                    $(skip).removeClass(classSkip).css("transition", "all 0.3s linear");
+                }, 1400);
+            }
+            setInterval(function () {
+                $(img).css({"opacity":"1","transition":"all 0.3s linear"});
+            }, 500);
+        }
         /*第二块*/
-        if (top >= sec_Top) {
-            $(".scl_tit").css({"opacity": "1", "margin-left": "0%", "transition": "all 0.6s linear"});
-            setInterval(function () {
-                $(".scl_titl_discri").css({"opacity": "1", "margin-left": "0%", "transition": "all 0.6s linear"});
-            }, 1000);
-            setInterval(function () {
-                $(".scl_skip").css({"opacity": "1", "margin-left": "0%", "transition": "all 0.6s linear"});
-            }, 2000)
-
-        }
+        myAni(sec_Top,".scl_tit",".scl_titl_discri",".scl_skip");
+        /*第三块*/
+        myAni(thi_Top,".thi_ani_tit",".thi_ani_discri",".thi_ani_skip");
         /*第四块*/
-        if (top >for_Top) {
-            $(".four_tit").css({"opacity": "1", "margin-left": "0%", "transition": "all 0.6s linear"});
-            setInterval(function () {
-                $(".four_titl_discri").css({"opacity": "1", "margin-left": "0%", "transition": "all 0.6s linear"});
-            }, 1000);
-            setInterval(function () {
-                $(".four_skip").css({"opacity": "1", "margin-left": "0%", "transition": "all 0.6s linear"});
-            }, 2000)
-
-        }
+        myAni(for_Top,".four_tit",".four_titl_discri",".four_skip");
+        /*第五块*/
+        myAni(fif_Top,".fifth_tit",".fifth_ani_discri",".fifth_ani_skip");
+        /*第六块*/
+        myAni(six_Top,".sixth_ani_tit",".sixth_ani_discri",".sixth_ani_skip");
     });
+    
+    function textAni() {
+        
+    }
 });
